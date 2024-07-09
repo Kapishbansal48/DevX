7df463b838c37506a067a9fa1e362611dbe7aa29
Kapishbansal48 <134728855+Kapishbansal48@users.noreply.github.com>
2024-07-09 14:51:12 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,13 @@
 import React,{useState, useEffect} from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp, faLocationDot, faPhone , faPaperPlane} from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp} from '@fortawesome/free-solid-svg-icons';
 // import "./footer.html"
 
 export default function Footer() {
 
     // Icons
     const faArrowUpIcon = <FontAwesomeIcon icon={faArrowUp} />
-     const faLocationDotIcon = <FontAwesomeIcon icon={faLocationDot} />
-    const faPhoneIcon = <FontAwesomeIcon icon={faPhone} />
-    const faPaperPlaneIcon = <FontAwesomeIcon icon={faPaperPlane} />
 
     // State
     const [isVisible, setIsVisible] = useState(false);
@@ -38,27 +35,6 @@ export default function Footer() {
     <section className="container">
         <footer className="footer">
             <p>Copyright © Trivion Technologies Private Limited</p> 
-            <div className="con">
-                       <p className='con1'> Trivion Technologies Private Limited</p>
-                    <div className="about_icon">
-                    {faLocationDotIcon}
-                    </div>
-                    <div className="about_content">
-                    <p className='con1'>G-03, Capital High Street, Mahal Road,Jaipur</p>
-                    </div>
-                    <div className="about_icon">
-                        {faPhoneIcon}
-                    </div>
-                    <div className="about_content">
-                    <p className='con2'>+91 80056 42075</p>
-                    </div>
-                    <div className="about_icon">
-                        {faPaperPlaneIcon}
-                    </div>
-                    <div className="about_content">
-                    <p className='con3'>hello@deevelox.com</p>    
-                    </div>
-             </div>       
         </footer>
     </section>
 
